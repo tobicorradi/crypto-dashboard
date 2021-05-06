ae9d0662e5df9c0087e91e5c290b5e9ae4fb2c7f
tobicorradi <corraditobias@gmail.com>
2021-05-06 17:11:20 -0300

    Header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Container from "./../Container";
+import $ from "styled-components";
+const $Header = $.div`
+    background-color: white;
+    padding: 25px 0;
+`;
 const Header = () => {
   return (
-    <header>
+    <$Header>
       <Container>
         <h1>
           Crypto Dashboard <span>by Toby Corradi</span>
         </h1>
       </Container>
-    </header>
+    </$Header>
   );
 };
 
