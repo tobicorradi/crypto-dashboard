d5a311d0b79f6b96f160f341c05137a3ba675ced
tobicorradi <corraditobias@gmail.com>
2021-05-06 16:39:39 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Crypto Dashboard <span>by Toby Corradi</span></h1>
+        </div>
+    )
+}
+
+export default App
