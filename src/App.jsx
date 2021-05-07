b5ec9b56a3c8bee1756fdede88b5998fd15204da
tobicorradi <corraditobias@gmail.com>
2021-05-06 22:25:27 -0300

    Card component created filled with content and props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import GlobalStyles from "./globalStyles";
 import CurrencyButton from "./components/CurrencyButton";
 import Container from "./components/Container";
+import Card from "./components/Card";
 const App = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const App = () => {
         <CurrencyButton text={"USD"} />
         <CurrencyButton text={"ARG"} />
         <CurrencyButton text={"BTC"} />
+        <Card name={'Bitcoin'} symbol={'BTC'} price={'50.405'}/>
       </Container>
     </>
   );
