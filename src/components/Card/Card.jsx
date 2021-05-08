1af33991b194db97b0fe7072d5e61f68c95c0325
tobicorradi <corraditobias@gmail.com>
2021-05-07 22:32:31 -0300

    Placeholder values for Card added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,75 +1,44 @@
 import React from "react";
-import { AreaChart, Area } from "recharts";
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
-const Card = ({ name, symbol, image, price }) => {
+import { AreaChart, Area, Tooltip } from "recharts";
+import $ from "styled-components";
+const $Card = $.div`
+  background-color: white;
+`;
+const $CardHeader = $.div`
+  padding: 20px 30px
+`;
+const Card = ({ name, symbol, image, price, data }) => {
   return (
-    <div>
-      <div>
-        <img src={image} alt="" />
-        <span>{name}</span>
-        <span>
-          <small>{symbol}</small>
-        </span>
-      </div>
-      <div>
-        <span>
-          {price} <span>USD</span>
-        </span>
-      </div>
-      <AreaChart
+    <$Card>
+      <$CardHeader>
+        <div>
+          <img src={image} alt="" />
+          <span>{name}</span>
+          <span>
+            <small>{symbol}</small>
+          </span>
+        </div>
+        <div>
+          <span>
+            {price} <span>USD</span>
+          </span>
+        </div>
+      </$CardHeader>
+      {/* <AreaChart
         width={300}
         height={100}
         data={data}
-        strokeWidth={4}
+        strokeWidth={3}
         syncId="anyId"
       >
-        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
-      </AreaChart>
-    </div>
+        <Area
+          type="monotone"
+          dataKey={''}
+          stroke="#82ca9d"
+          fill="#82ca9d"
+        />
+      </AreaChart> */}
+    </$Card>
   );
 };
 
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,12 +1,10 @@
+import axios from "axios";
 import React from "react";
 import Card from "./../Card";
 const Slider = () => {
   return (
     <div>
-      <Card name={"Bitcoin"} symbol={"BTC"} price={"50.405"} />
-      <Card name={"Bitcoin"} symbol={"BTC"} price={"50.405"} />
-      <Card name={"Bitcoin"} symbol={"BTC"} price={"50.405"} />
-      <Card name={"Bitcoin"} symbol={"BTC"} price={"50.405"} />
+      <Card name={'Bitcoin'} symbol={'BTC'} price={'50.345'}/>
     </div>
   );
 };
